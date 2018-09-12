23790c0fff619c7bca79fd338d8a363b4c746a5b
Abhinav Srivastava <36011576+AbhinavMir@users.noreply.github.com>
2018-09-12 11:34:21 +0530

    Updates

--- back.py
+++ back.py
@@ -1,9 +1,24 @@
 from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os
+
 app = Flask(__name__)
- 
-@app.route("/")
-def index():
-    return "Hello World!"
- 
+
+@app.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return "Hello Boss!"
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return home()
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=4000)
+    app.secret_key = os.urandom(12)
+    app.run(debug=True,host='0.0.0.0', port=4000)
