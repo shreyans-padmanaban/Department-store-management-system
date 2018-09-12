128f221bfbbbb855cbe443c748993e7d62d837f4
Abhinav Srivastava <36011576+AbhinavMir@users.noreply.github.com>
2018-09-13 01:22:13 +0530

    Updates

--- Db.py
+++ Db.py
@@ -3,16 +3,13 @@ from tkinter import *
 conn = sqlite3.connect('Emp.db')
 c=conn.cursor()
 
-master=Tk()
-
 def create_table():
     c.execute('CREATE TABLE IF NOT EXISTS customer(id INTEGER PRIMARY KEY,name TEXT,phone TEXT)')
     c.execute('CREATE TABLE IF NOT EXISTS bill(bill_no INTEGER,cust_id INTEGER,total INTEGER)')
-    c.execute('CREATE TABLE IF NOT EXISTS employee(emp_id INTEGER,name TEXT,mobile INTEGER,addr TEXT,sex TEXT,dob DATE,salary INTEGER,pass TEXT)')
+    c.execute('CREATE TABLE IF NOT EXISTS employee(DEPT TEXT,emp_id INTEGER,name TEXT,mobile INTEGER,addr TEXT,sex TEXT,dob DATE,salary INTEGER,pass TEXT)')
     c.execute('CREATE TABLE IF NOT EXISTS transactions(tran_id INTEGER,bill_no INTEGER,p_id INTEGER)')
     c.execute('CREATE TABLE IF NOT EXISTS product(p_id INTEGER PRIMARY KEY,name TEXT,stock INTEGER,exp_date DATE,mrp INTEGER,aisle_no INTEGER)')
 
 create_table()
-insert_table()
 conn.commit()
 conn.close()
