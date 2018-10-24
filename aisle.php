83d05804d9b9d7034e94f947e9cbee33eb770f70
Abhinav Srivastava <36011576+AbhinavMir@users.noreply.github.com>
2018-10-24 19:29:35 +0530

    Working update :)

--- aisle.php
+++ aisle.php
@@ -1,4 +1,6 @@
 <html>
+<a href="logout.php">Logout</a>
+<head>
 <style>
 @import url('https://fonts.googleapis.com/css?family=Quicksand');
 body{
@@ -16,13 +18,13 @@ div{
 p{
   font-weight: bold;
 }
-</style>
+</style></head>
 <body><div>
 <p><center><b><span style="font-size:35px;">Employer's login</b></center></p><br><br>
 <?php
 
 require_once('mysqli_connect.php');
-$query="SELECT name,department,position,address,id FROM employee;";
+$query="SELECT 'number','emp_id','stock','category' FROM aisle;";
 $response=@mysqli_query($dbc,$query);
 $row;
 
@@ -31,22 +33,19 @@ if($response){
   cellspacing="5" cellpadding="8">
 
   <tr>
-  <td align="left"><b>Name</b></td>
-  <td align="left"><b>Department</b></td>
-  <td align="left"><b>Position</b></td>
-  <td align="left"><b>Address</b></td>
-  <td align="left"><b>ID</b></td>';
+  <td align="left"><b>Aisle Number</b></td>
+  <td align="left"><b>Employee assigned</b></td>
+  <td align="left"><b>Supply(1=Stocked,0=Null)</b></td>
+  <td align="left"><b>Category</b></td>';
 
   while($row=mysqli_fetch_array($response)){
     echo '<tr><td align=left>'.
-    $row['name'] .'</td><td align="left">' .
-    $row['department'] .'</td><td align="left">' .
-    $row['position'] .'</td><td align="left">' .
-    $row['address'] .'</td><td align="left">' .
-    $row['id'] .'</td><td align="left">' ;
+    $row['number'] .'</td><td align="left">' .
+    $row['emp_id'] .'</td><td align="left">' .
+    $row['stock'] .'</td><td align="left">' .
+    $row['category'] .'</td><td align="left">' ;
     echo '</tr>';}
-    echo '<table>';
-  }
+    echo '<table>';}
 else{
   echo "Could not connect";
   echo mysqli_error($dbc);
--- mysqli_connect.php
+++ mysqli_connect.php
@@ -6,7 +6,7 @@ define('DB_NAME', 'store');
 
 /* Attempt to connect to MySQL database */
 $dbc = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
+$link=$dbc;
 // Check connection
 if($dbc === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
